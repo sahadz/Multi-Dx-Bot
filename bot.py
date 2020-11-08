0c5b7896df0ca4c0a09d721540fb15966dc0c7f5
ᴀɴᴀɴᴅ./ <62738084+Anandpskerala@users.noreply.github.com>
2020-11-08 12:46:45 +0530

    Bug fixes

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
+import os
 from config import Config
 from multidxbot import MultiDxBot
 
 
 if __name__ == "__main__":
+    if not os.path.exists(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
     MultiDxBot().run()
