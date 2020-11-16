93676dc25813e8f148e2e87be498f55153e37682
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2020-11-16 19:04:19 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -1,1 +1,8 @@
-skip
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+@Client.on_message(filters.command("start", prefixes=["/"]))
+async def start_message_handler(c: Client, m: Message):
+        await m.reply_text(
+            text=f"Hello! My name is Multi-DX😜"
+        )
